2b725eb800177d8a2586236aa76e98ceb78cbce5
frangimenezcaparros <frangimenezcaparros@gmail.com>
2019-12-13 08:57:03 +0100

    13/12/2019
    AI_Basico

---
+++ Assets/C#/MovementScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MovementScript : MonoBehaviour
+{
+    public GameObject destino;
+    private UnityEngine.AI.NavMeshAgent agenteDeNavegacion;
+
+    //Use this for initialization  
+    void Start()
+    {
+        agenteDeNavegacion = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agenteDeNavegacion.SetDestination(destino.transform.position);
+    }
+}
